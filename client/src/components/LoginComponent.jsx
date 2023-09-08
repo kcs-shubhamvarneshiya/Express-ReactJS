2490267124c9a9e4b95632439f61cb49890beec6
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-09-08 15:00:17 +0530

    include @style emotions lib

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import NavbarComponent from "./NavbarComponent";
 import userService from "../services/userService";
 import { isAxiosError } from "axios";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 export default function LoginComponent() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [open,setOpen] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -17,30 +20,32 @@ export default function LoginComponent() {
       formData.append("password", password);
 
       const result = await userService.login(formData);
-      console.log(result.data.Message)
+     
       setMessage(result.data.Message);
-
+      setOpen(true)
       
     } catch (error) {
       const err = isAxiosError(error);
-      console.log(error.response.data.msg)
       if (err) {
         setMessage(String(error?.response?.data?.msg));
+        setOpen(true)
       }
     }
-
-    setTimeout(() => {
-      setMessage("");
-    }, 5000);
-    event.target.reset();
   };
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
   return (
     <div>
       <NavbarComponent />
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+          <label htmlFor="exampleInputEmail1">Email address</label>
           <input
             type="email"
             className="form-control"
@@ -55,7 +60,7 @@ export default function LoginComponent() {
           </small>
         </div>
         <div className="form-group">
-          <label for="exampleInputPassword1">Password</label>
+          <label htmlFor="exampleInputPassword1">Password</label>
           <input
             type="password"
             className="form-control"
@@ -70,6 +75,20 @@ export default function LoginComponent() {
         </button>
       </form>
       <p>{message}</p>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000} // The duration the Snackbar will be displayed (in milliseconds)
+        onClose={handleClose}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          onClose={handleClose}
+          severity="success" // You can use 'success', 'error', 'warning', or 'info'
+        >
+          This is a Snackbar message!
+        </MuiAlert>
+      </Snackbar>
     </div>
   );
 }
