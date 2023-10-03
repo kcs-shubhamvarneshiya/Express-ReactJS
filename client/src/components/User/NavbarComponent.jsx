9551d7cd1b5a444bafb906457b79d9b4edcc2db5
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-10-03 15:48:58 +0530

    set profile toggle in user's navbar

--- client/src/components/User/NavbarComponent.jsx
+++ client/src/components/User/NavbarComponent.jsx
@@ -1,31 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../stylesheets/App.css";
+import WavesOutlinedIcon from "@mui/icons-material/WavesOutlined";
+import ProfileComponent from "../admin/ProfileComponent";
+
 
 export default function NavbarComponent(props) {
+  const [data, setData] = useState([]);
+  const [isProfileOptionVisible, setIsProfileOptionVisible] = useState(false);
 
+  useEffect(() => {
+    var data = localStorage.getItem("name");
+    data = data.replaceAll('"', "");
+    setData(data);
+  }, []);
+
+  const toggleProfileOptions = ()=>{
+    setIsProfileOptionVisible(!isProfileOptionVisible);
+}
 
   return (
     <div className="nav">
-  <input type="checkbox" id="nav-check"/>
-  <div className="nav-header">
-    <div className="nav-title">
-      SemiWaves
+      <input type="checkbox" id="nav-check" />
+      <div className="nav-header">
+        <div className="nav-title">
+          Semi
+          <span>
+            <WavesOutlinedIcon />
+          </span>
+        </div>
+      </div>
+      <div className="nav-btn">
+        <label htmlFor="nav-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+
+      <div className="nav-links">
+        <a href="/">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/product">Product</a>
+
+        {data ? (
+          <small onClick={toggleProfileOptions}>{data}</small>
+        ) : (
+          <>
+            <a href="/login">Login</a>
+            <a href="/login">Registration</a>
+          </>
+        )}
+      </div>
+
+      <div className="profile-component">
+        {isProfileOptionVisible && <ProfileComponent />}
+      </div>
     </div>
-  </div>
-  <div className="nav-btn">
-    <label htmlFor="nav-check">
-      <span></span>
-      <span></span>
-      <span></span>
-    </label>
-  </div>
-  
-  <div className="nav-links">
-    <a href="/">Home</a>
-    <a href="/about">About Us</a>
-    <a href="/product">Product</a>
-    <a href="/login">Login</a>
-  </div>
-</div>
   );
 }
