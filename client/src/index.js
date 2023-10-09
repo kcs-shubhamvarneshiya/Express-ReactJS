7b7d9d1eb1b8db54edc0b0a43834c6ad97fa94af
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-10-09 16:51:29 +0530

    include Product jsx

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ import AboutComponent from "./pages/user/AboutComponent";
 import MainComponent from "./pages/admin/MainComponent";
 import CategoryComponent from "./pages/admin/CategoryComponent";
 import AddProductComponent from "./components/admin/AddProductComponent";
+import Product from "./pages/admin/Product";
 
 export default function Index() {
   return (
@@ -20,7 +21,7 @@ export default function Index() {
         <Route path="/show" element={<ShowComponent />} />
         <Route path="/register" element={<RegisterComponent />} />
         <Route path="/admin" element={<MainComponent/>}/>
-        <Route path="/product" element={<AddProductComponent/>}/>
+        <Route path="/product" element={<Product/>}/>
         <Route path="/about" element={<AboutComponent/>}/>
         <Route path="/category" element={<CategoryComponent/>}/>
       </Routes>
