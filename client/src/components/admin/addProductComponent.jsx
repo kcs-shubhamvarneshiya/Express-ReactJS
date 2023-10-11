5d613325d88103cd06fdbc26c36f54bce25c032b
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-10-11 10:31:56 +0530

    define private route

--- client/src/components/admin/addProductComponent.jsx
+++ client/src/components/admin/addProductComponent.jsx
@@ -5,32 +5,34 @@ import { isAxiosError } from "axios";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 import productService from "../../services/product-service";
-import {errorHandler} from "../../helper/handler"
+import { errorHandler } from "../../helper/handler";
+import "../../stylesheets/App.css";
 import { useNavigate } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function AddProductComponent() {
-  const [productName, setProductName] = useState("");
-  const [productVarient, setProductVarient] = useState("");
-  const [productPrice, setProductPrice] = useState("");
-  const [productCamera, setProductCamera] = useState("");
-  const [ProductColor, setProductColor] = useState("");
-  const [productProcessor, setProductProcessor] = useState("");
-  const [productScreen, setProductScreen] = useState("");
-  const [productFrontImage, setProductFrontImage] = useState("");
-  const [productSideImage, setProductSideImage] = useState("");
-  const [productBackImage, setProductBackImage] = useState("");
-  const [category, setCategory] = useState("");
+  
+  const [isDisabled, setIsDisabled] = useState(false);
   const [resp, setResp] = useState("");
   const [data, setData] = useState("");
   const [resType, setResType] = useState("error");
   const [open, setOpen] = useState(false);
+
+  const [inputList, setInputList] = useState([
+    {
+      input: "",
+      input_rank: null
+    }
+  ])
 
   const fetchCategory = async () => {
     try {
       const res = await categoryService.getCategory();
       setData(res.Data);
     } catch (error) {
-      const err = errorHandler(error)  
+      const err = errorHandler(error);
       setResType("error");
       setResp(err);
       setOpen(true);
@@ -39,25 +41,17 @@ export default function AddProductComponent() {
 
   useEffect(() => {
     fetchCategory();
+    if (inputList.length > 0) {
+      inputList[inputList.length - 1].input === ""
+        ? setIsDisabled(true)
+        : setIsDisabled(false)
+    }
   }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-
       const formData = new FormData();
-      formData.append("productName", productName);
-      formData.append("Front_Image", productFrontImage);
-      formData.append("Back_Image", productBackImage);
-      formData.append("Side_Image", productSideImage);
-      formData.append("Varient", productVarient);
-      formData.append("Camera", productCamera);
-      formData.append("Processor", productProcessor);
-      formData.append("Screen", productScreen);
-      formData.append("productPrice", productPrice);
-      formData.append("productColor", ProductColor);
-      formData.append("categoryId", category);
-
       const result = await productService.insertProduct(formData);
       console.log(result);
 
@@ -78,127 +72,88 @@ export default function AddProductComponent() {
     setOpen(false);
   };
 
-  const onSelectHandler = (event) => {
-    console.log("values : ",event.target.value)
+  const handleRemove = (index) => {
+    const newList = [...inputList]
+    newList.splice(index, 1)
+    setInputList(newList)
   };
+
+  const handleInputChange = (event,index) => {
+    const { value } = event.target
+    const newInputList = [...inputList]
+    newInputList[index].input = value
+    newInputList[index].input_rank = index + 1
+    setInputList(newInputList)
+  };
+
+  const handleAdd = () => {
+    setInputList([
+      ...inputList,
+      {
+        input: "",
+        input_rank: null
+      }
+    ])
+  };
+
+  const onSelectHandler = (event) => {
+    console.log("values : ", event.target.value);
+  };
+
+  console.log(inputList)
 
   return (
     <>
-      <AdminNavbarComponent />
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div className="">
-            <input
-              type="text"
-              name="productName"
-              onChange={(event) => {
-                setProductName(event.target.value);
-              }}
-              required
-            />
-          </div>
+      <div className="insert-product">
+        <div className="product-container">
+          <form onSubmit={handleSubmit}>
 
-          <div className="">
-            <input
-              type="text"
-              name="productVarient"
-              onChange={(event) => {
-                setProductVarient(event.target.value);
-              }}
-            />
-          </div>
+            {/**start*/}
+            <div className="product-sub-container">
+              
+              {/*Heading start*/}
+              <div className="product-heading">
+                <p>
+                  Network <span>*</span>
+                </p>
+              </div>
+              {/*Heading end*/}
 
-          <div className="">
-            <input
-              type="text"
-              name="productColor"
-              onChange={(event) => {
-                setProductColor(event.target.value);
-              }}
-            />
-          </div>
+              <div className="product-suv-heading-container">
+                <div className="product-sub-heading">
+                  <p>Technology</p>
+                </div>
 
-          <div className="">
-            <input
-              type="text"
-              name="productPrice"
-              onChange={(event) => {
-                setProductPrice(event.target.value);
-              }}
-            />
-          </div>
+                {inputList.length > 0 ? inputList.map((input, index) => (
+                  <div key={index}>
+                  <div className="product-input-form">
+                  <TextField
+                  id="outlined-basic"
+                    name="networkName"
+                    variant="outlined"
+                    label={`Technology ${index+1}`}
+                    onChange={(event) => handleInputChange(event,index)}
+                  />
+                  <button onClick={() => handleRemove()}>
+                    <CloseIcon />
+                  </button>
+                  </div>
+                  </div>))
+                  : "No item in the list "}
 
-          <div className="">
-            <input
-              type="text"
-              name="productCamera"
-              onChange={(event) => {
-                setProductCamera(event.target.value);
-              }}
-            />
-          </div>
+                  
+                <div className="product-add-textbox-button">
+                  <button onClick={handleAdd} disabled={isDisabled}><AddIcon/></button>
+                </div>
 
-          <div className="">
-            <input
-              type="text"
-              name="productProcessor"
-              onChange={(event) => {
-                setProductProcessor(event.target.value);
-              }}
-            />
-          </div>
-
-          <div className="">
-            <input
-              type="text"
-              name="productScreen"
-              onChange={(event) => {
-                setProductScreen(event.target.value);
-              }}
-            />
-          </div>
-
-          <div className="">
-            <input
-              type="file"
-              name="productFrontImage"
-              onChange={(event) => {
-                setProductFrontImage(event.target.value);
-              }}
-            />
-          </div>
-
-          <div className="">
-            <input
-              type="file"
-              name="productSideImage"
-              onChange={(event) => {
-                setProductSideImage(event.target.value);
-              }}
-            />
-          </div>
-
-          <div className="">
-            <input
-              type="file"
-              name="productBackImage"
-              onChange={(event) => {
-                setProductBackImage(event.target.value);
-              }}
-            />
-          </div>
-
-          {/* <div>
-            <select onChange={onSelectHandler}>
-              <option>Please Select one category</option>
-              {data.map((data) => (
-                <option key={data._id}>{data.categoryName}</option>
-              ))}
-            </select>
-          </div> */}
-
-          <button>submit</button>
-        </form>
+            </div>
+             {/**End**/}       
+            <div className="form-submit">
+              <button>submit</button>
+            </div>
+        </div>
+          </form>
+        </div>
         <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
           <MuiAlert
             elevation={6}
